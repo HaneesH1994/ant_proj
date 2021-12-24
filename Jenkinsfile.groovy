194237e1b86326f0663f579c0134cba0252507fc
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2021-12-24 19:53:57 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,19 +1,17 @@
 node {
-    def ant
-    stage('Clone the code') {
-        git 'https://github.com/HaneesH1994/ant_proj.git'
-        ant = tool 'Ant'
-    }
-    stage('Build') {
-        withEnv(["Ant=$ant"]) {
-                sh "$Ant/bin/ant"
-        }
-    }
-    stage('Deploy the code') {
-        sh '''
-        pwd
-        ls -l
-        sudo cp /var/lib/jenkins/workspace/ant-scrptd/dist/AntExample.war /var/lib/tomcat8/webapps/
-        '''
-    }
-}
+   def myant
+     stage('clone the code') {
+	      git 'https://github.com/HaneesH1994/ant_proj.git'
+		  myant = tool 'Ant'
+	}
+      stage('Build the code') {
+	     withEnv(["usrant=$myant"]) {
+		       sh "$usrant/bin/ant"
+		}
+	}
+	 stage('Deploy the code') {
+	    sh '''
+		ls -l
+		'''
+	}
+}	
